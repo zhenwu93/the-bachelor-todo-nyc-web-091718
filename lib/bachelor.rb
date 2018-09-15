645c91e2de6c6cfc2a674a600dbfc0fb54fece27
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-15 05:52:57 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -37,7 +37,15 @@ end
 
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |person|
+      person.do |key, value|
+      if value == hometown
+        return person["occupation"]
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
