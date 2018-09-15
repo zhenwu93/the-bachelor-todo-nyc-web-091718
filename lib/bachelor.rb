38cfb0bf85a0fd8c19e6dfbf0f9fcf3859546d0a
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-15 05:50:36 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -22,8 +22,19 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, contestants|
+    contestants.each do |person|
+      person.each do |key, value|
+      if value == hometown
+        counter += 1
+        end
+      end
+    end
+  end
+  counter
 end
+
 
 def get_occupation(data, hometown)
   # code here
